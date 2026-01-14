42ea8f83e93c9080494691bd37d23cc335bbf8f7
shekelashvili-st <sergey200004@gmail.com>
2026-01-15 01:53:54 +0300

    Rename gateway to gateway-server for tests

--- infra/gateway-server/src/main/java/ru/yandex/practicum/infra/gateway/server/GatewayServer.java
+++ infra/gateway-server/src/main/java/ru/yandex/practicum/infra/gateway/server/GatewayServer.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.infra.gateway;
+package ru.yandex.practicum.infra.gateway.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Gateway {
+public class GatewayServer {
     public static void main(String[] args) {
-        SpringApplication.run(Gateway.class, args);
+        SpringApplication.run(GatewayServer.class, args);
     }
 }
