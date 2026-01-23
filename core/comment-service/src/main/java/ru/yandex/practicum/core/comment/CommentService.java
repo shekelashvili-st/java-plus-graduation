fb769b039bf9d9883f0e289f4159425116656f12
shekelashvili-st <sergey200004@gmail.com>
2026-01-23 03:38:50 +0300

    Update component scan directories

--- core/comment-service/src/main/java/ru/yandex/practicum/core/comment/CommentService.java
+++ core/comment-service/src/main/java/ru/yandex/practicum/core/comment/CommentService.java
@@ -7,7 +7,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication(scanBasePackages = {
         "ru.yandex.practicum.core.comment",
         "ru.yandex.practicum.stats.client",
-        "ru.yandex.practicum.core.common.aspect"
+        "ru.yandex.practicum.core.common.aspect",
+        "ru.yandex.practicum.core.common.client"
 })
 @EnableFeignClients(basePackages = "ru.yandex.practicum.core.common.client")
 public class CommentService {
--- core/event-service/src/main/java/ru/yandex/practicum/core/event/EventService.java
+++ core/event-service/src/main/java/ru/yandex/practicum/core/event/EventService.java
@@ -6,7 +6,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {
         "ru.yandex.practicum.core.event",
-        "ru.yandex.practicum.stats.client"
+        "ru.yandex.practicum.stats.client",
+        "ru.yandex.practicum.core.common.aspect",
+        "ru.yandex.practicum.core.common.client"
 })
 @EnableFeignClients(basePackages = "ru.yandex.practicum.core.common.client")
 public class EventService {
